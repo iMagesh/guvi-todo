05cc4b28befc7524877a2f7d2404eacc7348be53
Magesh <magesh@sedintechnologies.com>
2020-08-05 12:49:08 +0530

    App and index js files updated

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import TodoForm from "./TodoForm";
@@ -71,7 +46,6 @@ class App extends React.Component {
       </div>
     );
   }
->>>>>>> Stashed changes
 }
 
 export default App;
